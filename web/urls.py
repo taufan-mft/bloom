dc6c2fc061e2854862ca02a79f3ad048162e7709
Taufan MFT <taufanderazor@gmail.com>
2020-09-07 13:04:16 +0700

    seblak

--- web/urls.py
+++ web/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
  path('', views.home),
  path('second/', views.secondpage),
  path('seblak-bloom/', views.viewSeblak),
+ path('bloom-raw-honey/', views.viewMadu),
+ path('franchise/', views.viewFranchise),
+ path('daftar-outlet/', views.viewLokasiOutlet),
  path('tentang-kami/', views.viewTentang)
 ]
--- web/views.py
+++ web/views.py
@@ -13,5 +13,17 @@ def viewSeblak(request):
     return render(request, 'seblak.html')
 
 
+def viewMadu(request):
+    return render(request, 'madu.html')
+
+
+def viewFranchise(request):
+    return render(request, 'inner-page.html')
+
+
+def viewLokasiOutlet(request):
+    return render(request, 'lokasi_outlet.html')
+
+
 def viewTentang(request):
     return render(request, 'tentang-kami.html')
