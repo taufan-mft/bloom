e999d72a6f4b3f644f40ef262fe567a2dc808804
taufan_mft <taufanderazor@gmail.com>
2020-09-24 23:14:04 +0700

    ubah debug false

--- bloom/settings.py
+++ bloom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'wa*zt(j_!urtc=mmh2%*+8@@rfsuhk)j82+7tjrqk7pzs&ul-w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
